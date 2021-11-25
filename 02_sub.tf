25776626e0f1c7f3a304305ff1d634e16a7b2bd5
sdkim <ksd7248@gmail.com>
2021-11-25 12:27:47 +0900

    'step1'

---
+++ 02_sub.tf
@@ -0,0 +1,54 @@
+# 가용 영역 a의 public subnet
+resource "aws_subnet" "sdkim_puba" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "sdkim-puba"
+  }
+}
+
+resource "aws_subnet" "sdkim_pubc" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "sdkim-pubc"
+  }
+}
+
+resource "aws_subnet" "sdkim_pria" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "sdkim-pria"
+  }
+}
+
+resource "aws_subnet" "sdkim_pric" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "sdkim-pric"
+  }
+}
+
+resource "aws_subnet" "sdkim_pridba" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "sdkim-pridba"
+  }
+}
+
+resource "aws_subnet" "sdkim_pridbc" {
+  vpc_id = aws_vpc.sdkim_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "sdkim-pridbc"
+  }
+}
