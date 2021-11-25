1310a464a7926beb5406bf990eae60ccc360c403
sdkim <ksd7248@gmail.com>
2021-11-25 12:29:21 +0900

    'step2'

--- 02_sub.tf
+++ 02_sub.tf
@@ -1,7 +1,7 @@
 # 가용 영역 a의 public subnet
 resource "aws_subnet" "sdkim_puba" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-northeast-2a"
   tags = {
     "Name" = "sdkim-puba"
@@ -9,8 +9,8 @@ resource "aws_subnet" "sdkim_puba" {
 }
 
 resource "aws_subnet" "sdkim_pubc" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "sdkim-pubc"
@@ -18,8 +18,8 @@ resource "aws_subnet" "sdkim_pubc" {
 }
 
 resource "aws_subnet" "sdkim_pria" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-northeast-2a"
   tags = {
     "Name" = "sdkim-pria"
@@ -27,8 +27,8 @@ resource "aws_subnet" "sdkim_pria" {
 }
 
 resource "aws_subnet" "sdkim_pric" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "sdkim-pric"
@@ -36,8 +36,8 @@ resource "aws_subnet" "sdkim_pric" {
 }
 
 resource "aws_subnet" "sdkim_pridba" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-northeast-2a"
   tags = {
     "Name" = "sdkim-pridba"
@@ -45,8 +45,8 @@ resource "aws_subnet" "sdkim_pridba" {
 }
 
 resource "aws_subnet" "sdkim_pridbc" {
-  vpc_id = aws_vpc.sdkim_vpc.id
-  cidr_block = "10.0.5.0/24"
+  vpc_id            = aws_vpc.sdkim_vpc.id
+  cidr_block        = "10.0.5.0/24"
   availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "sdkim-pridbc"
