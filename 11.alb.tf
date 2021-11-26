da459017875b6190cb4c88fe38daad3225936e42
sdkim <ksd7248@gmail.com>
2021-11-26 12:16:14 +0900

    'step1'

---
+++ 11.alb.tf
@@ -0,0 +1,10 @@
+resource "aws_lb" "sdkim_alb" {
+  name               = "sdkim-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sdkim_sg.id]
+  subnets            = [aws_subnet.sdkim_puba.id,aws_subnet.sdkim_pubc.id]
+  tags = {
+    "Name" = "sdkim-alb"
+  }
+}
