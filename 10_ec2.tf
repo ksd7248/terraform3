42f0c7def5c38137e1c540d84c614332c1896938
sdkim <ksd7248@gmail.com>
2021-11-26 12:04:23 +0900

    'step2'

--- 10_ec2.tf
+++ 10_ec2.tf
@@ -15,24 +15,24 @@ data "aws_ami" "amzn" {
 }
 
 resource "aws_instance" "sdkim_web" {
- ami  =  data.aws_ami.amzn.id
- instance_type = "t2.micro"
- key_name = "sdkim-key"
- vpc_security_group_ids = [aws_security_group.sdkim_sg.id]
- availability_zone = "ap-northeast-2a"
- private_ip = "10.0.0.11"
- subnet_id = aws_subnet.sdkim_puba.id
+  ami                    = data.aws_ami.amzn.id
+  instance_type          = "t2.micro"
+  key_name               = "sdkim-key"
+  vpc_security_group_ids = [aws_security_group.sdkim_sg.id]
+  availability_zone      = "ap-northeast-2a"
+  private_ip             = "10.0.0.11"
+  subnet_id              = aws_subnet.sdkim_puba.id
 
- tags = {
-   "Name" = "sdkim-web"
- }
+  tags = {
+    "Name" = "sdkim-web"
+  }
 }
 
 resource "aws_eip" "sdkim_web_ip" {
-  vpc = true
-  instance = aws_instance.sdkim_web.id
+  vpc                       = true
+  instance                  = aws_instance.sdkim_web.id
   associate_with_private_ip = "10.0.0.11"
-  depends_on = [aws_internet_gateway.sdkim_ig]
+  depends_on                = [aws_internet_gateway.sdkim_ig]
 }
 
 output "public_ip" {
